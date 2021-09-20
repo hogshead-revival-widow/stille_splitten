6b8367bdd367b73dab702906b28f644d2a111f87
hogshead-revival-widow <67462744+hogshead-revival-widow@users.noreply.github.com>
2021-09-20 17:51:50 +0200

    -run_batch; dopplung bei setup entfernt; besseres handling UNWEIGHTED_RESULTS

--- stille_splitten/split_silence.py
+++ stille_splitten/split_silence.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from .consts import SUCCESS, NO_RESULT, UNWEIGHTED_RESULTS
 from .settings import SETTINGS
 from .sequences import search_sequences, display_sequences
-from .helper import setup, get_batch_files
+from .helper import get_batch_files, setup
 
 def display_results(file_name_analyzed, expectation, sequences, plausibility):
     """ Zeigt gefundene Ergebnisse mit weiterführenden Informationen an.
@@ -25,17 +25,23 @@ def display_results(file_name_analyzed, expectation, sequences, plausibility):
             Gefundene Sequenzen: {len(sequences)} (erwartet: {expectation})''')
         display_sequences(file_name_analyzed, plausibility, sequences)
     elif UNWEIGHTED_RESULTS < plausibility < SUCCESS:
+        str_expectation = '(erwartet: {expectation})'
+        if expectation == 0:
+            str_expectation = '(keine Erwartung angegeben)'
         print(f'''Wahrscheinlicher Fund für: {file_name}.
-            Gefundene Sequenzen: {len(sequences)} (erwartet: {expectation})
-            Zuverlässigkeit: {plausibility}x (bei {len(SETTINGS['ffmpeg_options'])} Gesamtdurchläufen) die gleiche Sequenz-Menge gefunden.''')
+            Gefundene Sequenzen: {len(sequences)} {str_expectation}
+            Zuverlässigkeit: {plausibility}x (bei {len(SETTINGS['ffmpeg_options'])} Durchläufen) die gleiche Sequenz-Menge gefunden.''')
         display_sequences(file_name_analyzed, plausibility, sequences)
     elif plausibility == UNWEIGHTED_RESULTS:
         print(f'''Achtung!
             Keine Bewertung der Funde für: {file_name} möglich.
-            Zeige alle unbewerteten Funde:''')
+            Zeige alle unbewerteten Funde mit Sequenzmengen:''')
         for number, sequences in enumerate(sequences, 1):
-            print(f'''{number}: Gefundene Sequenzen: {len(sequences)}''')
+            print(f'''
+            {number}: Gefundene Sequenzen: {len(sequences)}''')
             display_sequences(file_name_analyzed, plausibility, sequences)
+        if SETTINGS['batch_processing']:
+            print(f'''\nHinweis: Diese Sequenzen werden zur näheren Prüfung alle in eine separate Dateien (Präfix: "unschluessig") geschrieben.''')
     elif plausibility == NO_RESULT:
         print(f'''Achtung!
             Kein Fund für: {file_name}.
@@ -43,42 +49,46 @@ def display_results(file_name_analyzed, expectation, sequences, plausibility):
     else:
         raise ValueError(
             f'Unerwartetes Ergebnis in plausibility: {plausibility}')
-    print('\n---')
-
-def run_batch(from_dir):
-    """ Beginnt die Stapelverarbeitung.
-    
-    Args:
-        * from_dir: str/Path
-    """
-    setup()
-    
-    logger = logging.getLogger(__name__)
-    logger.info(f'Stapelverarbeitung: Ergebnisaufgabe auf Sammeldatei gesetzt.')
-    SETTINGS['batch_processing'] = True
-
-    if from_dir is None:
-        from_dir = SETTINGS["dir_batch_processing"]
-    paired_files = get_batch_files(from_dir)
-
-    run(paired_files)
 
 
-def run(paired_files):
+def run(files_from, cli_input=None):
     """ Beginnt Suchprozess in `paired_files`.
 
     Args:
-        * paired_files: [(Datei:Path, Erwartete_Sequenzen:int)]  
+        * files_from: 
+            * entweder: [(Datei:Path, Erwartete_Sequenzen:int)]  
+            * oder: Path/str
     """
-    setup()
-    
+
+    setup(cli_input)
+
     logger = logging.getLogger(__name__)
-    logger.debug(f'Stapelverarbeitung: input: {paired_files}')
+    paired_files = files_from
+
+    if not isinstance(paired_files, list):
+        logger.info(f'Nehme an, dass Stapelverarbeitung')
+        logger.debug(f'files_from input: {files_from}')
+        print('Bereite Stapelverarbeitung vor.')
+        paired_files = get_batch_files(files_from)
+    else:
+        logger.info(f'Nehme an, dass Einzeldatei-Verarbeitung')
+    
+
+    if len(paired_files) == 0:
+        logger.info(f'Beende: Keine Datei gefunden.\n\n')
+        print('Problem: Keine Dateien gefunden.')
+        if SETTINGS['batch_processing']:
+            print(f'Sind Dateien im Verzeichnis `{SETTINGS["dir_batch_processing"]}`?')
+        exit('Beeendet.')
+
     logger.info(f'Beginne Suche (Lauf-ID: {SETTINGS["run_id"]}).')
     print(f'Beginne Suche (Lauf-ID: {SETTINGS["run_id"]}).')
-    for file_name, expectation in paired_files:
+    for file_counter, (file_name, expectation) in enumerate(paired_files, 1):
         try:
-            print(f'Durchsuche: {file_name}')
+            nr_of = ''
+            if file_counter != len(paired_files):
+                nr_of = f'({file_counter}/{len(paired_files)})'
+            print(f'Durchsuche: {file_name} {nr_of}')
             print('Das kann einen Moment dauern, bitte warten.')
             all_sequences, plausibility = search_sequences(file_name, expectation)
             display_results(file_name, expectation, all_sequences, plausibility)
@@ -87,5 +97,6 @@ def run(paired_files):
                 logger.debug(f'input: {paired_files}')
                 logger.debug(critical_error, exc_info=True)
                 logger.error(f'Breche Suche für {file_name.stem} ab.\nGrund: {critical_error} (vgl. Log).')
+        print('---\n')
 
-    print('Beendet!')
+    print('Beendet.')
