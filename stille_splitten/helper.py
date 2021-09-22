bb8d8cfb35b453205842373e85dfd8e8186fa6be
hogshead-revival-widow <67462744+hogshead-revival-widow@users.noreply.github.com>
2021-09-22 23:17:35 +0200

    0.0.5

--- stille_splitten/helper.py
+++ stille_splitten/helper.py
@@ -4,6 +4,7 @@ import string
 from pathlib import Path
 
 from .settings import SETTINGS
+from .consts import NAME
 
 
 def setup(cli_input=None):
@@ -15,10 +16,10 @@ def setup(cli_input=None):
     setup_from_cli(cli_input)
     setup_logger()
 
+
 def setup_from_cli(cli_input):
- 
     if cli_input is None:
-         return
+        return
 
     SETTINGS['cli_input'] = cli_input
 
@@ -26,22 +27,14 @@ def setup_from_cli(cli_input):
         SETTINGS['log_level'] = logging.DEBUG
 
 
-
 def setup_directories():
-    """ Prüft ob die in `SETTINGS` genannten Verzeichnisse existieren,
-    wenn nicht, werden sie angelegt. """
     all_dirs = [key for key in SETTINGS if key.startswith('dir')]
     for dir in all_dirs:
         Path(SETTINGS[dir]).mkdir(parents=True, exist_ok=True)
 
 
 def setup_logger():
-    """ Initalisiert den Logger mit Werten aus:
-
-        * `SETTINGS['log_level']`
-        * `SETTINGS['log_file'])` (Path)
-    """
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger(NAME)
     logger.setLevel(SETTINGS['log_level'])
     fh = logging.FileHandler(SETTINGS['log_file'])
     fh.setLevel(SETTINGS['log_level'])
@@ -49,18 +42,19 @@ def setup_logger():
         '%(asctime)s | %(levelname)-8s | %(funcName)s |  %(lineno)04d | %(message)s')
     fh.setFormatter(formatter)
     logger.addHandler(fh)
-    logger.info(f'Setup erfolgreich. Laufe mit den folgenden Einstellungen: {SETTINGS}')
+    logger.info(
+        f'Setup erfolgreich. Laufe mit den folgenden Einstellungen: {SETTINGS}')
 
 
 def check_input(from_file, expectation):
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger(NAME)
     logger.debug(f'input: {from_file, expectation, from_file}')
 
     if not Path(from_file).is_file():
-        is_not_file = f'Die angegebene Audiodatei ("{from_file}") ist keine Datei.'
+        is_not_file = f'Suche soll in ("{from_file}") stattinden. Aber das ist keine Datei/Verzeichnis.'
         raise Exception(is_not_file)
     if not isinstance(expectation, int):
-        is_not_number = f'Der angegebene Wert erwarteter Korpusse ("{expectation}") ist keine Zahl.'
+        is_not_number = f'Der angegebene Wert erwarteter Sequenzen ("{expectation}") ist keine Zahl.'
         raise Exception(is_not_number)
 
 
@@ -75,7 +69,7 @@ def is_plausible_result(found_sequences, expectation):
     Returns:
         * bool
     """
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger(NAME)
     logger.debug(
         f'input: {found_sequences, expectation}')
 
@@ -89,6 +83,7 @@ def is_plausible_result(found_sequences, expectation):
         f'Unplausibel. Erwartet: {expectation}. Gefunden: {found_sequences}')
     return False
 
+
 def get_batch_files(from_dir):
     """ Liest alle Dateien mit ggf. im Dateinamen vermerkter
     Erwartung an Sequenzen in Datei aus `SETTINGS['dir_batch_processing']`.
@@ -98,7 +93,7 @@ def get_batch_files(from_dir):
     Returns:
         * paired_files: [(Datei:Path, Erwartete_Sequenzen:int)]  
     """
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger(NAME)
     logger.info('Stapelverarbeitung: Suche Files')
     logger.info(f'Stapelverarbeitung: Ergebnisaufgabe auf Sammeldatei gesetzt.')
     SETTINGS['batch_processing'] = True
@@ -116,23 +111,23 @@ def get_batch_files(from_dir):
                 expectation = int(file_name.split('-')[0])
                 logger.debug(f'Gefundene Erwartung: {expectation}')
             except Exception as non_critical_error:
-                logger.debug(f'Fehler beim Auslesen von Erwartung aus Dateiname, Fehler: {non_critical_error}')
-                print('Namenskonvention: <Erwartung>-abc.endung')
-                print('Beispiel: 3-W0543125.mp3 (Erwartung: 3 Sequenzen)')
-                print(f'Aus der Datei {in_file_name} konnte keine Erwartung gelesen werden.')
-                print('Fahre ohne Erwartung fort.')
+                logger.debug(
+                    f'Fehler beim Auslesen von Erwartung aus Dateiname, Fehler: {non_critical_error}')
+                print(f'''Aus der Datei {in_file_name} konnte keine Erwartung gelesen werden.
+                    Hinweis:
+                    * Namenskonvention: <Erwartung>-abc.endung
+                    * Beispiel: 3-W0543125.mp3 (Erwartung: 3 Sequenzen)')''')
+                print('Durchsuche diese Datei ohne Erwartung an eine Anzahl von Sequenzen.')
                 return 0
             return expectation
         return 0
-            
-        
+
     paired_files = [(item, get_expectation(item)) for item in Path(from_dir).glob('**/*.*')
                     if item.is_file() and not item.stem.startswith('.')]
     logger.info(
         f'Stapelverarbeitung: Files gefunden \n Gefundene Files:\n{paired_files}')
     return paired_files
 
-def get_run_id(length=8):
-    """ Generiert eine ID der Länge `length`."""
-    return ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(length))
 
+def get_run_id(length=8):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(length))
