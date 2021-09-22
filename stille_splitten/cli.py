6cd5f932c9122201b381b877b14e3adb4d7ff3da
hogshead-revival-widow <67462744+hogshead-revival-widow@users.noreply.github.com>
2021-09-22 23:16:10 +0200

    0.0.5

--- stille_splitten/cli.py
+++ stille_splitten/cli.py
@@ -1,29 +1,27 @@
-#!/usr/bin/env python3
-
 import click
-import logging
 from pathlib import Path
 from .settings import SETTINGS
 from .split_silence import run
+from .consts import VERSION, NAME
 
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
-exec_name = 'stille_splitten' # 'stille.exe'
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help', '--hilfe'])
+
 
 def start_processing(**kwargs):
-   
+
     if kwargs['batch_processing']:
         files_from = kwargs['verzeichnis']
     else:
         expectation = 0
         if kwargs['erwartung'] is not None:
-             expectation = kwargs['erwartung']
+            expectation = kwargs['erwartung']
         files_from = [(Path(kwargs['datei']), expectation)]
-    
+
     run(files_from, cli_input=kwargs)
 
 
 @click.group(context_settings=CONTEXT_SETTINGS)
-@click.version_option(version='0.0.3', help='Version anzeigen und beenden.')
+@click.version_option(version=VERSION, help='Version anzeigen und beenden.')
 def cli(**kwargs):
     """Findet länger anhaltende Stille in Audiofiles und generiert daraus Sequenzen."""
     pass
@@ -38,8 +36,8 @@ def cli(**kwargs):
  * Gibt es keine mit einem Bindestrich getrennte Zahl im Dateinamen,
    wird ohne Erwartung gesucht.
  
- Beispielaufruf: `{exec_name} stapel`
- Durchsuche anderes Verzeichnis: `{exec_name} stapel "aus_diesem_verzeichnis"`
+ Beispielaufruf: `{NAME} stapel`
+ Durchsuche anderes Verzeichnis: `{NAME} stapel "aus_diesem_verzeichnis"`
  \n
  ''')
 @click.argument('verzeichnis', required=False, type=click.Path(exists=True))
@@ -50,10 +48,11 @@ def stapel(**kwargs):
     kwargs['batch_processing'] = True
     start_processing(**kwargs)
 
+
 @cli.command(short_help=f'''Sucht in <Datei> nach Sequenzen, optional mit der Angabe der Anzahl der erwarteten Sequenzen (<Erwartung>).\n
 \b
-Beispielaufruf: `{exec_name} datei "abc.mp3"`
-Aufruf mit Erwartung: `{exec_name} datei "abc.mp3" 13`
+Beispielaufruf: `{NAME} datei "abc.mp3"`
+Aufruf mit Erwartung: `{NAME} datei "abc.mp3" 13`
 \b\n
 ''')
 @click.argument('datei', type=click.Path(exists=True))
@@ -63,5 +62,3 @@ def datei(**kwargs):
     """Sucht in <Datei> nach Sequenzen, optional mit der Angabe der Anzahl der erwarteten Sequenzen (<Erwartung>)"""
     kwargs['batch_processing'] = False
     start_processing(**kwargs)
-
-
