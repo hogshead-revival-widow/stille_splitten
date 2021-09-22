42c8e866c3ec0fd6dea569b3cfce549a256c6d91
hogshead-revival-widow <67462744+hogshead-revival-widow@users.noreply.github.com>
2021-09-22 23:14:55 +0200

    0.0.5

--- setup.py
+++ setup.py
@@ -1,38 +1,22 @@
-#!/usr/bin/env python3
-
-
 from setuptools import setup, find_packages
-import sys
+from stille_splitten.consts import VERSION, NAME
 
-
-#ffmpeg_file = 'ffmpeg'
-#if 'win32' in sys.platform.lower():
-#    ffmpeg_file = 'ffmpeg.exe'
+with open('requirements.txt', 'r') as f:
+    install_requires = f.read().splitlines()
 
 setup(
-    name='stille_splitten',
-    version='0.0.3',
+    name=NAME,
+    version=VERSION,
     description='Splittet Beiträge die durch Stille separiert sind',
     packages=find_packages(),
     include_package_data=True,
-    #package_data={'stille_splitten': ['bin/'+ffmpeg_file]},
-    python_requires='>=3.8.2',
+    python_requires='~=3.6.8',
     classifiers=['Development Status :: 3 - Alpha',
                  'Programming Language :: Python', 'Programming Language :: Python :: 3',
                  'Topic :: Multimedia :: Sound/Audio', 'Topic :: Multimedia :: Sound/Audio :: Analysis',
                  'Topic :: Utilities'],
-    install_requires=['click==8.0.1',
-                      'et-xmlfile==1.1.0',
-                      'ffmpeg-python==0.2.0',
-                      'future==0.18.2',
-                      'numpy==1.21.2',
-                      'openpyxl==3.0.8',
-                      'pandas==1.3.3',
-                      'python-dateutil==2.8.2',
-                      'pytz==2021.1',
-                      'six==1.16.0',
-                      'timecode==1.3.1'],
+    install_requires=install_requires,
     entry_points={
-        "console_scripts": ["stille_splitten = stille_splitten.cli:cli"]
-    },
+        "console_scripts": [f"{NAME} = stille_splitten.cli:cli"]
+    }
 )
