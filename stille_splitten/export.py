2b23e4413aaa36af77f5e808cb473a76886db2e5
hogshead-revival-widow <67462744+hogshead-revival-widow@users.noreply.github.com>
2021-09-22 23:16:56 +0200

    0.0.5

--- stille_splitten/export.py
+++ stille_splitten/export.py
@@ -2,9 +2,10 @@ import logging
 import json
 from pathlib import Path
 from timecode import Timecode
-import pandas as pd
+from openpyxl import Workbook, load_workbook
 from .settings import SETTINGS
-from .consts import UNWEIGHTED_RESULTS
+from .consts import UNWEIGHTED_RESULTS, NAME
+
 
 def serialize_timecode(tc):
     if not isinstance(tc, Timecode):
@@ -15,20 +16,21 @@ def serialize_timecode(tc):
 def write_batch_results(file_name_analyzed, plausibility, sequences):
     """ Schreibt errechnete Ergebnisse in Sammeldateien (JSON/Excel),
     je eine für alle in diesem Stapelverarbeitungsprozess analysierten Dateien.
-    
+
     Args:
         * file_name_analyzed: Path
         * plausibility: int
         * sequences: [dict,..]
     """
 
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger(NAME)
     logger.info('Schreibe Ergebnis in Sammeldatei')
+    logger.debug(f'Exportiere: {sequences}')
 
     to_dir = SETTINGS['dir_results']
 
     file_name_id = SETTINGS['run_id']
-    
+
     if plausibility == UNWEIGHTED_RESULTS:
         file_name_results = f'unschluessig_{file_name_analyzed.stem}_{file_name_id}'
     else:
@@ -37,76 +39,83 @@ def write_batch_results(file_name_analyzed, plausibility, sequences):
     excel_file = Path(to_dir) / Path(f'{file_name_results}.xlsx')
     json_file = Path(to_dir) / Path(f'{file_name_results}.json')
 
-    
-
     for item in sequences:
         item.update(
-            dict(sicherheit=plausibility,
-                 datei=str(file_name_analyzed.stem))
+            dict(plausibility=plausibility,
+                 in_file=str(file_name_analyzed.stem))
         )
 
-    results = pd.DataFrame(sequences)
+    sequences_to_json(json_file, sequences)
+    sequences_to_excel(excel_file, sequences)
+
+
+def sequences_to_excel(excel_file, sequences):
 
     if Path(excel_file).is_file():
-        df_excel = pd.read_excel(excel_file)
-        result = pd.concat([df_excel, results], ignore_index=True)
-        result.to_excel(excel_file, index=False)
+        wb = load_workbook(excel_file)
     else:
-        writer = pd.ExcelWriter(excel_file)
-        results.to_excel(writer, index=False)
-        writer.save()
+        wb = Workbook()
+    ws = wb.active
 
+    row = len(list(ws.rows))
+
+    if row == 1:
+        ws.cell(row=row, column=1, value='Untersuchte Datei')
+        ws.cell(row=row, column=2, value='Korpusnummer')
+        ws.cell(row=row, column=3, value='Typ')
+        ws.cell(row=row, column=4, value='Wert')
+
+    for sequence in sequences:
+        for key in sequence:
+            row += 1
+            ws.cell(row=row, column=1, value=sequence['in_file'])
+            ws.cell(row=row, column=2, value=sequence['sequence_nr'])
+            ws.cell(row=row, column=3, value=key)
+            ws.cell(row=row, column=4, value=serialize_timecode(sequence[key]))
+
+    wb.save(excel_file)
+
+
+def sequences_to_json(json_file, sequences):
+    file_data = [sequences]
     if Path(json_file).is_file():
         with open(json_file, 'r') as f:
             file_data = json.load(f)
         file_data.append(sequences)
-        with open(json_file, 'w') as f:
-            json_out = json.dump(file_data, f, indent=4, default=serialize_timecode)
-    else:
-        with open(json_file, 'w') as f:
-            json.dump([sequences], f, indent=4, default=serialize_timecode)
+    with open(json_file, 'w') as f:
+        json.dump(file_data, f, indent=4, default=serialize_timecode)
 
 
 def write_single_results(file_name_analyzed, plausibility, sequences):
     """ Schreibt errechnete Ergebnisse in Einzeldateien (JSON/Excel)
     pro zu analysierende Datei.
-    
+
     Args:
         * file_name_analyzed: Path
         * plausibility: int
         * sequences: [dict,..]
     """
-    
-    logger = logging.getLogger(__name__)
-    logger.info('Schreibe Ergebnis in Einzeldateien')
 
+    logger = logging.getLogger(NAME)
+    logger.info('Schreibe Ergebnis in Einzeldateien')
+    logger.debug(f'Exportiere: {sequences}')
     to_dir = SETTINGS['dir_results']
 
     file_name_id = SETTINGS['run_id']
     file_name_results = f'{Path(file_name_analyzed).stem}_{file_name_id}'
-
 
     excel_file = Path(to_dir) / Path(f'{file_name_results}.xlsx')
     json_file = Path(to_dir) / Path(f'{file_name_results}.json')
 
     for item in sequences:
         item.update(
-            dict(sicherheit=plausibility,
-                 datei=str(file_name_analyzed.stem)
-        ))
+            dict(plausibility=plausibility,
+                 in_file=str(file_name_analyzed.stem)
+                 ))
 
-    results = pd.DataFrame(sequences)
+    sequences_to_json(json_file, sequences)
+    sequences_to_excel(excel_file, sequences)
 
-    writer = pd.ExcelWriter(excel_file)
-    results.to_excel(writer, index=False)
-    writer.save()
-
-    with open(json_file, 'w') as f:
-            json_out = json.dumps([sequences], indent=4, default=serialize_timecode)
-            f.write(json_out)
-    
-
-    
 
 def write_results(file_name_analyzed, plausibility, sequences):
     """ Löst entweder Sammel- oder Einzeldateien-Export der Ergebnisse aus
@@ -117,7 +126,7 @@ def write_results(file_name_analyzed, plausibility, sequences):
         * plausibility: int
         * sequences: [dict,..]
     """
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger(NAME)
     logger.debug(
         f'input: {file_name_analyzed, plausibility, sequences}')
 
@@ -130,4 +139,3 @@ def write_results(file_name_analyzed, plausibility, sequences):
         return None
 
     write_single_results(file_name_analyzed, plausibility, sequences)
-
