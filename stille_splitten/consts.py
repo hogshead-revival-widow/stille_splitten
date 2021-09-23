8567b3830ea8c2f0476c4e75613fbd1e78e031e1
hogshead-revival-widow <67462744+hogshead-revival-widow@users.noreply.github.com>
2021-09-23 18:48:59 +0200

    + pyinstall-support

--- stille_splitten/consts.py
+++ stille_splitten/consts.py
@@ -1,5 +1,16 @@
+# status der zurückgegebenen sequenz
 SUCCESS = 100
 NO_RESULT = -1
 UNWEIGHTED_RESULTS = 1
-VERSION = '0.0.5'
-NAME = 'stille_splitten'
+
+# programm
+VERSION = '0.0.6'
+NAME = 'stille' # Name des zu generierenden Konsolenskripts
+
+""" Wenn `FFMPEG_FILE` None ist, wird `ffmpeg` im Pfad erwartet.
+Andernfalls kann hier die systemspezifische ffmpeg-binary in bin/ abgelegt werden,
+dann muss (systemunabhängig) angegeben werden: 'bin/<ffmpeg-datei>'. Wird von `SETTINGS` dann als 
+package ressource abgerufen."""
+FFMPEG_FILE = None
+
+PYINSTALL_PLANNED = False
